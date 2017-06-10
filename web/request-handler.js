9c664b826ba80553d3f41d8d71078052c24521d9
Hack Reactor Student <student@hackreactor.com>
2017-06-09 18:54:09 -0700

    Implement archived site redirect fulfillment

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,15 +1,34 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelper = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.writeHead(200, httpHelper.headers);
   if (req.method === 'GET') {
-    httpHelper.serveAssets(res, archive.paths.siteAssets + '/index.html', () => {
+    res.writeHead(200, httpHelper.headers);
+    if (req.url === '/' || req.url === '/index.html') {
+      url = archive.paths.siteAssets + '/index.html';
+    } else {
+      url = archive.paths.archivedSites + req.url;
+    }
+      
+    httpHelper.serveAssets(res, url, () => {
       console.log('index.html served');
     }); 
+
   }
+  
+  // if (req.method === 'POST') {
+  //   req.on('data', (chunk) => {
+  //     ({url} = querystring.parse(chunk.toString('utf8')));
+  //     if (archive.isUrlArchived(url, () => {})) {
+  //       res.writeHead(301, {location: req.url + '/' + url});
+  //       console.log(req.headers);
+  //       // httpHelper.serveAssets(res, archive.paths.archivedSites + url, () => console.log(url, ' was served'));
+  //     }
+  //   });
+  // }
 
   //res.end(archive.paths.list);
 };
